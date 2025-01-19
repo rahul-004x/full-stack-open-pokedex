d8080b60ecbb4899123a22c2286e0753465be29d
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-19 14:27:41 +0530

    fix lint error

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require("express")
+import express from 'express'
+import dotenv from 'dotenv'
+import process from 'process'
+
+dotenv.config()
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
