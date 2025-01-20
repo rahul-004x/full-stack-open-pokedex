c8e0173377fd0a5861d03917e1215003b53eb6b6
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-20 20:16:34 +0530

    fix test conflict

--- e2e/pokemon.spec.js
+++ e2e/pokemon.spec.js
@@ -1,15 +1,18 @@
-import { test, describe, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-describe('Pokedex', () => {
+test.describe('Pokedex', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:8080')
+  })
+
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
   test('clicking a pokemon shows its details', async ({ page }) => {
-    await page.goto('http://localhost:8080/')
     await page.getByText('ivysaur').click()
+    await page.waitForLoadState('networkidle')
     await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
