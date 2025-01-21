1fb8d3fb2cf644c6e4151c0ab14bf46fc5d0275b
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-21 22:54:46 +0530

    http_services.checks add on

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ import process from 'process'
 dotenv.config()
 const app = express()
 
+// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('version: 0.0.1') // change this string to ensure a new version deployedmething broke!')
+  res.send('New version is deployed') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
