1eace00bee181a327c4851b916b997966edd6ea6
rahul-004x <142470408+rahul-004x@users.noreply.github.com>
2025-01-21 22:16:48 +0530

    FLY deployments added

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ import process from 'process'
 dotenv.config()
 const app = express()
 
-// get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('') // change this string to ensure a new version deployed
+  res.send('version: 0.0.1') // change this string to ensure a new version deployedmething broke!')
 })
 
 app.listen(PORT, () => {
